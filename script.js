28319dfc93c519922c17d49b399731753e226726
Christopher Cummins <christopher.francis.cummins@gmail.com>
2021-07-03 17:02:38 -0400

    day one late afternoon updates

--- script.js
+++ script.js
@@ -21,36 +21,43 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "paper") {
         result = "You lose! Paper covers rock.";
         console.log(result);
+        scoreSummary(result);
         return "computer";
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
         result = "You win! Rock breaks scissors.";
         console.log(result);
+        scoreSummary(result);
         return "player";
     }   
     else if (playerSelection == "paper" && computerSelection == "rock") {
         result = "You win! Paper covers rock.";
         console.log(result);
+        scoreSummary(result);
         return "player";
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
         result = "You lose! Scissors cut paper.";
         console.log(result);
+        scoreSummary(result);
         return "computer";
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
         result = "You win! Scissors cut paper.";
         console.log(result);
+        scoreSummary(result);
         return "player";
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
         result = "You lose! Rock breaks scissors.";
         console.log(result);
+        scoreSummary(result);
         return "computer";
     }
     else {
         result = "It's a draw!";
         console.log(result);
+        scoreSummary(result);
     }
 }
 
@@ -71,11 +78,41 @@ function buttonGame() {
     console.log("Player Score: " + playerScore);
     console.log("Computer Score " + computerScore);
 
+
+    playerScoreDisplay(playerScore)
+    
+    computerScoreDisplay(computerScore)
+
     computerSelection = computerPlay ()
 }
 
+function scoreSummary(result) {
+    const scoreSummary = document.querySelector('#scoreSummary');
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.textContent = result;
+    scoreSummary.appendChild(content);
+}
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', buttonGame);
 });
+
+function playerScoreDisplay(playerScore) {
+    const playerScoreBoard = document.querySelector('#playerScoreBoard');
+    const score = document.createElement('div');
+    score.classList.add('score');
+    score.textContent = playerScore;
+    playerScoreBoard.appendChild(score);
+
+}
+
+function computerScoreDisplay(computerScore) {
+    const computerScoreBoard = document.querySelector('#computerScoreBoard');
+    const score = document.createElement('div');
+    score.classList.add('score');
+    score.textContent = computerScore;
+    computerScoreBoard.appendChild(score);
+
+}
